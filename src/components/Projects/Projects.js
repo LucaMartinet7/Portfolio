a58eadce3845bb59495f20d792060af9f6dea2c9
Luca Martinet <luca.martinet@epitech.eu>
2025-01-23 21:50:08 +0100

    feat: add carousel for projects no more than 2 per page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import arcade from "../../Assets/Projects/arcade.jpg";
@@ -19,47 +19,52 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Rtype}
-              isBlog={false}
-              title="BeeR-Type (R-Type)"
-              description="R-Type is an Epitech project for third-year level. It consists of creating a multiplayer 2D game inspired by classic arcade shoot-'em-ups, designed with a modular architecture. The project separates the server, client, and game engine to ensure scalability and maintainability while leveraging modern C++ standards, the Entity-Component-System (ECS) architecture, and robust networking protocols."
-              ghLink="https://github.com/lucamartinet7/R-Type"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={area}
-              isBlog={false}
-              title="AREA"
-              description="AREA is an Epitech project. We had to recreate the environment of IFTTT, with a backend, a web frontend and a mobile frontend."
-              ghLink="https://github.com/LucaMartinet7/Area-Tek3"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={neural}
-              isBlog={false}
-              title="Neural Network"
-              description="Neural Networks is an Epitech project for third year level, which consists to create neural network that can take a chess board as input, and outputs the status of the game using machine-learning-based solution."
-              ghLink="https://github.com/LucaMartinet7/Neural-Network"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={arcade}
-              isBlog={false}
-              title="Arcade"
-              description="The Arcade is an Epitech school project. It consists of creating a platform that allows you to play games with different graphic libraries and switch them at runtime. The project uses .so files to load the games dynamically."
-              ghLink="https://github.com/LucaMartinet7/Arcade"
-            />
-          </Col>
-        </Row>
+        <Carousel>
+          <Carousel.Item>
+            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={Rtype}
+                  isBlog={false}
+                  title="BeeR-Type (R-Type)"
+                  description="R-Type is an Epitech project for third-year level. It consists of creating a multiplayer 2D game inspired by classic arcade shoot-'em-ups, designed with a modular architecture. The project separates the server, client, and game engine to ensure scalability and maintainability while leveraging modern C++ standards, the Entity-Component-System (ECS) architecture, and robust networking protocols."
+                  ghLink="https://github.com/lucamartinet7/R-Type"
+                />
+              </Col>
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={area}
+                  isBlog={false}
+                  title="AREA"
+                  description="AREA is an Epitech project. We had to recreate the environment of IFTTT, with a backend, a web frontend and a mobile frontend."
+                  ghLink="https://github.com/LucaMartinet7/Area-Tek3"
+                />
+              </Col>
+            </Row>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={neural}
+                  isBlog={false}
+                  title="Neural Network"
+                  description="Neural Networks is an Epitech project for third year level, which consists to create neural network that can take a chess board as input, and outputs the status of the game using machine-learning-based solution."
+                  ghLink="https://github.com/LucaMartinet7/Neural-Network"
+                />
+              </Col>
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={arcade}
+                  isBlog={false}
+                  title="Arcade"
+                  description="The Arcade is an Epitech school project. It consists of creating a platform that allows you to play games with different graphic libraries and switch them at runtime. The project uses .so files to load the games dynamically."
+                  ghLink="https://github.com/LucaMartinet7/Arcade"
+                />
+              </Col>
+            </Row>
+          </Carousel.Item>
+        </Carousel>
       </Container>
     </Container>
   );
